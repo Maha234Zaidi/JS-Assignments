7f8e1ad7f29b43c5a2a37c1ea89330b9fda4cf29
Maha_Zaidi <124033522+Maha234Zaidi@users.noreply.github.com>
2023-04-05 04:21:29 +0500

    Add files via upload

---
+++ javascript/assests/css/JS/app.js
@@ -0,0 +1,18 @@
+
+//   Question no:01 
+// alert("Hello How Are You...!");
+
+// QUESTION NO:02
+
+// QUESTION NO:03
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+// QUESTION NO:04
+// alert("welcome to JS Land..");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs");
+
+// QUESTION NO:05
+// alert("Hello...I can run JS through my web browser's console")
+
+
+
