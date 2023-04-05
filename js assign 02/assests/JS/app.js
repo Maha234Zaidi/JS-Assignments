e7c27d7ac24d8d4da6d5a6702335787891c7ef91
Maha_Zaidi <124033522+Maha234Zaidi@users.noreply.github.com>
2023-04-05 05:31:10 +0500

    Add files via upload

---
+++ js assign 02/assests/JS/app.js
@@ -0,0 +1,41 @@
+//  QUESTION NO:01
+let username;
+
+// QUESTION NO:02
+let myName="MAHA ZAIDI";
+
+// QUESTION NO:03
+let titleMessage;
+titleMessage ="HELLO WORLD!";
+alert(titleMessage);
+
+// QUESTION NO:04
+let fullName = "MAHA ZAIDI";
+let age = "22";
+let course = "Certified Mobile Application Development";
+let mySubject ="Javascript";
+
+alert(fullName);
+alert(age);
+alert(course);
+alert(mySubject);
+
+//QUESTION NO:05
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// QUESTION NO:06
+let email="Mraaj130@gmail.com";
+alert("My email address is "  + email);
+
+// QUESTION NO:07
+let book;
+book="A smarter way to learn JavaScript";
+alert("I am trying to learn from the box " + book);
+
+// QUESTION NO:09
+let pattern;
+pattern = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(pattern);
+
+
